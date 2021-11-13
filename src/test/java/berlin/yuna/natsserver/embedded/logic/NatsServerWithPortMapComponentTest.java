9335d989a4eb4be777f37d63dbaa17f5f95e63fe
Yuna Morgenstern <io@yuna.berlin>
2021-11-13 23:17:25 +0100

    chore: cleanup code

--- src/test/java/berlin/yuna/natsserver/embedded/logic/NatsServerWithPortMapComponentTest.java
+++ src/test/java/berlin/yuna/natsserver/embedded/logic/NatsServerWithPortMapComponentTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
